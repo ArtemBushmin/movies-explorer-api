cc936a262bd87a9323109d755f45cb4bc6d4ccfc
Artem Bushmin <nim-shub@rambler.ru>
2022-09-09 20:22:18 +0300

    Бэкенд готов 4

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,8 @@ userRoutes.patch(
   '/me',
   celebrate({
     body: Joi.object().keys({
+      email: Joi.string().required().email(),
       name: Joi.string().min(2).max(30).required(),
-      about: Joi.string().min(2).max(30).required(),
     }),
   }),
   updateUserProfile,
